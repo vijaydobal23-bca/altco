c096c2439534f76e675aabcdad6af5b44b2d97e4
vijay singh dobal <vijaydobal23@gmail.com>
2026-07-22 13:21:53 +0530

    Change SMTP port to 443

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 443,
+  secure: true,
   auth: {
     user: process.env.GOOGLE_USER,
     pass: process.env.GMAIL_APP_PASS,
