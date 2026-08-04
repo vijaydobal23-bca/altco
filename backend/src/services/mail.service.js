0661ca8bb5636a49e7d824b00020082e6450d907
vijay singh dobal <vijaydobal23@gmail.com>
2026-08-04 13:12:18 +0530

    Update nodemailer configuration with tls options

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -1,19 +1,26 @@
 import nodemailer from "nodemailer";
 import dns from "dns";
 
-// Force IPv4 resolution to prevent IPv6 timeouts on Render
+// Force IPv4 resolution to prevent IPv6 issues on Render
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
+  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.GOOGLE_USER,
     pass: process.env.GMAIL_APP_PASS,
   },
+
   logger: true,
   debug: true,
+
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 transporter
@@ -22,8 +29,9 @@ transporter
     console.log("Transporter is ready");
   })
   .catch((err) => {
-    console.log("Email transporter varification failed", err);
+    console.log("Email transporter verification failed", err);
   });
+
 
 export async function sendEmail({ to, subject, html, text }) {
   const mailOptions = {
@@ -35,6 +43,7 @@ export async function sendEmail({ to, subject, html, text }) {
   };
 
   const details = await transporter.sendMail(mailOptions);
+
   console.log("Email sent", details);
 
   return details;
