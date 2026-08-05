2e78f6f4da9e09220734edec712508cd4d8d1f95
vijay singh dobal <vijaydobal23@gmail.com>
2026-08-05 16:24:01 +0530

    chore: add render url to cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,9 +16,9 @@ app.use(morgan("dev"));
 // ─── Core Middleware ──────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin:["http://localhost:5173"],
-    credentials:true
-   }),
+    origin: ["http://localhost:5173", "https://altco-2.onrender.com"],
+    credentials: true
+  }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
