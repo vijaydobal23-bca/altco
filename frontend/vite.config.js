295a94191ffdae64832082b4e1590cc836d60e4a
vijay singh dobal <vijaydobal23@gmail.com>
2026-08-05 16:32:13 +0530

    chore: remove custom build config from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,24 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor-react';
-            }
-            if (id.includes('gsap')) {
-              return 'vendor-gsap';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
 })
